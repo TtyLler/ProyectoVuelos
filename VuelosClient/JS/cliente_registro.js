1746121df3dbb205f20fac9b9ef60ed644ccb3f5
HyperP2502 <charly.sell@outlook.com>
2022-08-20 05:28:03 -0600

    Cambios de prueba

--- VuelosClient/JS/cliente_registro.js
+++ VuelosClient/JS/cliente_registro.js
@@ -32,25 +32,25 @@ function registration(e){
 
   const preformDatos = new FormData(form_registro);
   const formDatos = new URLSearchParams(preformDatos);
-  const nombre = document.getElementById("input_nombre").value;
-  const apellido1 = document.getElementById("input_apellido1").value;
-  const apellido2 = document.getElementById("input_apellido2").value;
-  const correo = document.getElementById("input_correo").value;
-  const usuario = document.getElementById("input_usuarioRegistro").value;
-  const constrasena = document.getElementById("input_passRegistro").value;
+  const name = document.getElementById("input_nombre").value;
+  const last_name1 = document.getElementById("input_apellido1").value;
+  const last_name2 = document.getElementById("input_apellido2").value;
+  const email = document.getElementById("input_correo").value;
+  const user = document.getElementById("input_usuarioRegistro").value;
+  const password = document.getElementById("input_passRegistro").value;
   let datos = {
-    nombre: nombre,
-    apellido1: apellido1,
-    apellido2: apellido2,
-    correo: correo,
-    usuario1: usuario,
-    constrasena: constrasena,
-    rol: 2,
-    rolNavigation: null
+    nombre: name,
+    apellido1: last_name1,
+    apellido2: last_name2,
+    correo: email,
+    usuario1: user,
+    constrasena: password,
+    rol: 2
   }
 
-  if(!nombre.value || !apellido1.value || !correo.value || !usuario.value || !constrasena.value){
-    alert('Llene todos los campos!')
+  if(!name || !last_name1 || !email || !user || !password ){
+    //alert('Ingrese todos los datos necesarios!')
+    alert(datos.constrasena)
   }else{
     e.preventDefault();
     fetch(API_URL, {
@@ -63,7 +63,20 @@ function registration(e){
   
     .then( response => response.json())
     .then( data => usuarios.push(data))
+    .then(
+      alert('Usuario registrado correctamente!'),
+      clear()
+    )
   }
+}
+
+function clear(){
+  document.getElementById("input_nombre").value = "",
+  document.getElementById("input_apellido1").value = "",
+  document.getElementById("input_apellido2").value = "",
+  document.getElementById("input_correo").value = "",
+  document.getElementById("input_usuarioRegistro").value = "",
+  document.getElementById("input_passRegistro").value = ""
 }
 
 getRegistro();
