86236ddb320f560ca572f8f8f0bd701b788ce57c
Roy Ureña <roy_urena12@hotmail.com>
2022-08-24 20:50:34 -0600

    Crear usuario y contrasena en admin  Borrados

--- VuelosClient/JS/compra.js
+++ VuelosClient/JS/compra.js
@@ -60,7 +60,41 @@ function SeleccionarVuelo(monto){
     document.getElementById('cantidadMonto').value = monto;
 }
 
-//Confirmar compra
+//Tarjeta
 
+const cleaveCC = new Cleave("#cardNumber", {
+  creditCard: true,
+  delimiter: "-",
+  onCreditCardTypeChanged: function (type) {
+    const cardBrand = document.getElementById("cardBrand"),
+      visa = "fab fa-cc-visa",
+      mastercard = "fab fa-cc-mastercard",
+      amex = "fab fa-cc-amex"
 
-  
+    switch (type) {
+      case "visa":
+        cardBrand.setAttribute("class", visa);
+        break;
+      case "mastercard":
+        cardBrand.setAttribute("class", mastercard);
+        break;
+      case "amex":
+        cardBrand.setAttribute("class", amex);
+        break;
+      default:
+        cardBrand.setAttribute("class", "");
+        break;
+    }
+  },
+});
+
+const cleaveDate = new Cleave("#cardExpiry", {
+  date: true,
+  datePattern: ["m", "y"],
+});
+
+const cleaveCCV = new Cleave("#cardCcv", {
+  blocks: [3],
+});
+
+//Captcha
